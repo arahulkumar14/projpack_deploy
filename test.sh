5a0618d29f55cecc107b46c39e21d85070da2ddd
arahulkumar14 <rkumara@axway.com>
2024-08-09 11:14:07 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 echo "Starting Deployment !"
 pwd
+ls
 cd /opt/Axway-7.7.0/apigateway/posix/bin
+ls
 pwd
-./projddeploy --dir=/jenagent/workspace/projpac_deploy --passphrase=none --name=deploypackage --type=fed --deploy-to --host-name=10.128.150.82 --port=8090 --user-name=admin --password==changeme --group-name=mygroup --includes server-1 
+./projdeploy --dir=/jenagent/workspace/projpac_deploy --passphrase=none --name=deploypackage --type=fed --deploy-to --host-name=10.128.150.82 --port=8090 --user-name=admin --password==changeme --group-name=mygroup --includes server-1 
