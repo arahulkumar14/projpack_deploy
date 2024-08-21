db4d47047ad5554e8cb9f03495f880c94b7823b5
arahulkumar14 <rkumara@axway.com>
2024-08-21 12:38:58 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 echo "Starting Deployment !"
 pwd
-ls
+String jobName = System.getenv('JOB_NAME')
 cd /opt/Axway-7.7.0/apigateway/posix/bin
-ls
 pwd
-./projdeploy --dir=/jenagent/workspace/proj_pack_deployment --passphrase-none --name=deploypackage --type=fed --deploy-to --host-name=10.128.150.82 --port=8090 --user-name=admin --password=changeme --group-name=mygroup --includes server-1
+echo "Starting Package creation"
+./projpack --create --dir=/jenagent/workspace/${jobName} --passphrase-none --name=deploymentpackage --type=fed --add ./cfd17dd7-0637-438a-8d97-a8d522d8fd44 --projpass-none
+./projdeploy --dir=/jenagent/workspace/${jobName} --passphrase-none --name=deploymentpackage --type=fed --deploy-to --host-name=10.128.150.82 --port=8090 --user-name=admin --password=changeme --group-name=mygroup --includes server-1
+echo "Deployment Completed!"
